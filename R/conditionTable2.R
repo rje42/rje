88a000bdf9d2e5d2b618a14b8340eeb149f39b54
Robin Evans <evans@stats.ox.ac.uk>
2016-08-15 17:27:32 -0400

    amended conditionTable2 to preserve dimension names

--- R/conditionTable2.R
+++ R/conditionTable2.R
@@ -4,7 +4,9 @@ function (x, variables, condition, undef=NaN)
 {
     out = conditionTable(x, variables, condition, undef=undef)
     tmp = patternRepeat0(c(variables, condition), dim(x), keep.order = TRUE)
-    return(array(out[tmp], dim(x)))
+    out = array(out[tmp], dim(x))
+    dimnames(out) = dimnames(x)
+    out
 }
 
 #' @export condition.table2
